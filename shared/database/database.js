4bb87d107224754f25b41766a77a867274434c9c
sebas <sebaslpz645.sl@gmail.com>
2024-06-17 17:07:41 -0600

    add validBase64 database.js

--- shared/database/database.js
+++ shared/database/database.js
@@ -3,10 +3,22 @@ import { Sequelize } from 'sequelize'
 
 dotenv.config()
 
-// Decodificar valores codificados en Base64
-const databaseName = atob(process.env.DATABASE_NAME);
-const databaseUser = atob(process.env.DATABASE_USER);
-const databasePassword = atob(process.env.DATABASE_PASSWORD);
+const isValidBase64String = (str) => {
+  const base64Regex = /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/;
+  return base64Regex.test(str);
+}
+
+const databaseName = isValidBase64String(process.env.DATABASE_NAME)
+  ? atob(process.env.DATABASE_NAME)
+  : null;
+
+const databaseUser = isValidBase64String(process.env.DATABASE_USER)
+  ? atob(process.env.DATABASE_USER)
+  : null;
+
+const databasePassword = isValidBase64String(process.env.DATABASE_PASSWORD)
+  ? atob(process.env.DATABASE_PASSWORD)
+  : null;
 
 const sequelize = new Sequelize('test-db', databaseUser, databasePassword, {
   dialect: 'sqlite',
