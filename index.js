490df14cd0421632076b3a1d0d2e36fb3af61026
sebas <sebaslpz645.sl@gmail.com>
2024-06-16 23:19:56 -0600

    add config to certs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import sequelize from './shared/database/database.js'
 import { usersRouter } from "./users/router.js"
 import express from 'express'
+import https from 'https'
+import fs from 'fs'
 
 const app = express()
 const PORT = 8000
@@ -9,6 +11,13 @@ sequelize.sync({ force: true }).then(() => console.log('db is ready'))
 
 app.use(express.json())
 app.use('/api/users', usersRouter)
+
+const options = {
+  key: fs.readFileSync('/etc/tls/tls.key'),
+  cert: fs.readFileSync('/etc/tls/tls.crt')
+};
+
+const server = https.createServer(options, app);
 
 const server = app.listen(PORT, () => {
     console.log('Server running on port PORT', PORT)
