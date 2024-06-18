d5900875a964c5cf853e7cf9d6f6a076ab356636
sebas <sebaslpz645.sl@gmail.com>
2024-06-17 18:29:10 -0600

    add again .evn

--- shared/database/database.js
+++ shared/database/database.js
@@ -3,26 +3,9 @@ import { Sequelize } from 'sequelize'
 
 dotenv.config()
 
-const isValidBase64String = (str) => {
-  const base64Regex = /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/;
-  return base64Regex.test(str);
-}
-
-const databaseName = isValidBase64String(process.env.DATABASE_NAME)
-  ? atob(process.env.DATABASE_NAME)
-  : null;
-
-const databaseUser = isValidBase64String(process.env.DATABASE_USER)
-  ? atob(process.env.DATABASE_USER)
-  : null;
-
-const databasePassword = isValidBase64String(process.env.DATABASE_PASSWORD)
-  ? atob(process.env.DATABASE_PASSWORD)
-  : null;
-
-const sequelize = new Sequelize('test-db', databaseUser, databasePassword, {
+const sequelize = new Sequelize('test-db', process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
   dialect: 'sqlite',
-  host: databaseName
+  host: process.env.DATABASE_NAME
 })
 
 export default sequelize
